61376a67dc07e18743fad1a68fbff7f76c939d3e
uuyb <uuyb@pc2013081010eny>
2016-05-17 15:16:30 +0800

    12

--- skylucene/src/com/dao/BaseDao.java
+++ skylucene/src/com/dao/BaseDao.java
@@ -5,10 +5,7 @@ import java.util.List;
 public interface BaseDao<T> {
     
     public void save(T t);
-    
-    
-    
-    
+     
     public void delete(T t);
     
     public void update(T t);
